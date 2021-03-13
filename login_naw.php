d5d52994c207736b4f5f609b5777f04066f0e5b8
Natchapon <theyakza15@hotmail.com>
2021-03-13 11:31:34 +0700

    12/03/64/002

--- login_naw.php
+++ login_naw.php
@@ -25,14 +25,16 @@
         <!-- Outer Row -->
         <div class="row justify-content-center  w-100">
 
-            <div class="col-xl-5 col-lg-7 col-md-9">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            
-                            <div class="col-lg-12">
+                        <div class="col-lg-7 ">
+                            <img height="350px" width="550px"src="images/login-bg.PNG" alt="">
+                            </div>
+                            <div class="col-lg-5">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-2">Mafear Clothing</h1>
